455d5c0dcde13059e61588cd5628b30f367d48eb
vitrich <ve@vitrich.ru>
2025-12-30 12:52:10 +0300

    assign

--- letovo/urls.py
+++ letovo/urls.py
@@ -5,8 +5,8 @@ from django.conf.urls.i18n import i18n_patterns
 urlpatterns = []
 
 urlpatterns += i18n_patterns(
+    path('assignments/', include('assignments.urls')),
     path('admin/', admin.site.urls),
     path("grade5/", include("grade5.urls")),
     path('', include('cms.urls')),   # стартовый URL — корень сайта
-    path('assignments/', include('assignments.urls')),
 )
