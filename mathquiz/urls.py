55617201eb44d51cd3ab7e0f817ac832e89d5c3e
vitrich <ve@vitrich.ru>
2025-12-31 12:40:13 +0300

    mathquiz 1

--- mathquiz/urls.py
+++ mathquiz/urls.py
@@ -4,9 +4,15 @@ from . import views
 app_name = 'mathquiz'
 
 urlpatterns = [
-    path('tasks/', views.task_list, name='task_list'),
+    # Главная страница quiz
+    path('', views.task_list, name='task_list'),
+
+    # Задания (альтернативный путь)
+    path('tasks/', views.task_list, name='task_list_alt'),
     path('tasks/<int:task_id>/', views.task_detail, name='task_detail'),
     path('tasks/<int:task_id>/submit/', views.submit_answer, name='submit_answer'),
+
+    # Личные кабинеты
     path('student-dashboard/', views.student_dashboard, name='student_dashboard'),
     path('teacher-dashboard/', views.teacher_dashboard, name='teacher_dashboard'),
 ]
